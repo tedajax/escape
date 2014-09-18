4c77ba8c050efa79f05b65c70184ad866f9475df
Ted Dobyns <ted@tedajax.net>
2014-09-17 23:06:33 -0700

    json start

---
+++ src/json.c
@@ -0,0 +1,9 @@
+#include "json.h"
+
+char *json_load_file(const char *filename) {
+
+}
+
+jsmntok_t *json_tokenize(char *js) {
+    
+}
---
+++ src/json.h
@@ -0,0 +1,22 @@
+#ifndef _JSON_H_
+#define _JSON_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "types.h"
+#include "vector.h"
+#include "hashtable.h"
+
+#define JSMN_STRING
+#define JSMN_PARENT_LINKS
+
+#include "jsmn.h"
+
+#define MAX_JSON_TOKENS 256
+
+char *json_load_file(const char *filename);
+jsmntok_t *json_tokenize(char *js);
+
+#endif
