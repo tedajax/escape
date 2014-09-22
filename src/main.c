49e40579e132472aaf8366bddc0aa4b058978da0
Ted Dobyns <ted@tedajax.net>
2014-09-21 18:10:41 -0700

    free the game

--- src/main.c
+++ src/main.c
@@ -2,18 +2,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "types.h"
-#include "containers.h"
-#include "tables.h"
-#include "parser.h"
-#include "json.h"
-#include "room.h"
 #include "game.h"
 
 int main(int argc, char *argv[]) {
     Game *game = game_new();
-
     int result = game_run(game, argc, argv);
-
+    game_free(game);
     return result;
 }
